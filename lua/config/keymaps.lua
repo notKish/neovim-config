03818e25a101c806b893dbcd42c4f4711e2e9117
notKish <ganesh3075@gmail.com>
2025-07-08 23:27:57 +0530

    update to new keymaps

--- lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv'")
-vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv'")
